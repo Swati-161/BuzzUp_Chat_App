dd998fa972981fafcbe8cad046e8d8a8ade73146
Swati-161 <nimswati7@gmail.com>
2025-07-25 19:13:28 +0530

    try1

--- my-chat-app/src/firebase.js
+++ my-chat-app/src/firebase.js
@@ -5,7 +5,7 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-  // databaseURL: "https://buzzup-724ca-default-rtdb.asia-southeast1.firebasedatabase.app",
+  databaseURL: "https://buzzup-724ca-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
